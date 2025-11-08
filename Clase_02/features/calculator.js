95200dd28d9873125f9668540beec723c1002412
Sergio_Andres_Lopez_Sepulveda <ser.and60@gmail.com>
2025-11-08 12:29:27 -0500

    feat(calc): add sum function

---
+++ Clase_02/features/calculator.js
@@ -0,0 +1,9 @@
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+
+const valor1 = parseFloat(prompt("Elije el primer valor: "));
+const valor2 = parseFloat(prompt("Elije el segundo valor: "));
+
+const resultado = valor1 + valor2;
+
+console.log(resultado);
